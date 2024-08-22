8ca52a08662766e4f1960e09fb23a6fad2b286e7
politan <politan@protonmail.com>
2024-08-22 23:00:07 +0200

    add typescript support and dotenv package

---
+++ api/src/app.ts
@@ -0,0 +1,43 @@
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = process.env;
+
+const server: FastifyInstance = Fastify({ logger: true });
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
+
+server.get('/ping', opts, async (request, reply) => {
+  return { pong: 'it worked!' }
+})
+
+const start = async () => {
+  try {
+    await server.listen({ port: Number(env.SERVER_PORT) || 5000 })
+
+    const address = server.server.address()
+    const port = typeof address === 'string' ? address : address?.port
+
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
