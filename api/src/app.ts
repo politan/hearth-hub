c5c21faa98f6d51b3e5f41461fa8a26a3537b582
politan <politan@protonmail.com>
2024-08-22 23:42:59 +0200

    update Fastify import in app.ts and fix linting issues in eslint.config.mjs

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,4 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
-import { Server, IncomingMessage, ServerResponse } from 'http';
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -21,26 +20,26 @@ const opts: RouteShorthandOptions = {
       }
     }
   }
-}
+};
 
 server.get('/ping', opts, async (request, reply) => {
   reply.code(200);
-  return { pong: 'it worked!' }
-})
+  return { pong: 'it worked!' };
+});
 
 const start = async () => {
   try {
-    await server.listen({ port: Number(env.SERVER_PORT) || 5000 })
+    await server.listen({ port: Number(env.SERVER_PORT) || 5000 });
 
-    const address = server.server.address()
-    const port = typeof address === 'string' ? address : address?.port
+    const address = server.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
 
   } catch (err) {
-    server.log.error(err)
-    process.exit(1)
+    server.log.error(err);
+    process.exit(1);
   }
 }
 
 console.log('test');
 
-start()
+start();
